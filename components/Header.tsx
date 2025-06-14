e896f38c8b5f538c0e1efe9f41a32c8692b4e174
saulm23 <marcasaul00@gmail.com>
2025-06-14 09:22:05 -0400

    Added components and header

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <Link href="/About">
+        </Link>
+    </div>
+  )
+}
+
+export default Header
