d54a76a26b502d60f78b59abe75924b56e9b8dd5
saulm23 <marcasaul00@gmail.com>
2025-06-14 09:45:16 -0400

    Added (routes)

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+
+    <div>
+      
+      HomePage</div>
   )
 }
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,24 @@
+'use client'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const Header = () => {
+    const route = useRouter();
   return (
-    <div>
-        <Link href="/About">
+    <div className="flex items-center">
+        
+        <Link href="">
+        about
         </Link>
+        <Link href ="/Dashboard">
+            dashboard
+        </Link>
+        <Link href = "/">
+        home
+        </Link>
+
+        
     </div>
   )
 }
